911126772d231b4fc6b5ef32fc8b69c845e3c472
Manas Ranjan Adha <manas865873@gmail.com>
2024-12-01 23:16:37 +0530

    integrate clerk auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+import { SignedIn } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return (
+      <SignedIn />
+    )
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div>
+  
+    { userId && <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -96,6 +108,8 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+    </div>}
+
     </div>
   );
 }
